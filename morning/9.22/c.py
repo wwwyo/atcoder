f22db5ba91c5077dfadcf9acc38a945f4708c28e
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-09-23 11:22:38 +0900

    bfs

--- morning/9.22/c.py
+++ morning/9.22/c.py
@@ -1,16 +1,57 @@
 # https://atcoder.jp/contests/abc211/tasks/abc211_d
+# 方針
+# 1. 探索するpath
+# 2. 頂点1からのstep数を管理、チェックもできる
+# 3. 頂点1からのその経路に訪れるルートの数。前の経路のパスと同じ数になる
+# 今回は最短経路の数を求めたいから探索済のpathの扱いに注意する
+# 前通りとっても O(N+M)
+# 各頂点をリストに入れる数はN
+# Nからの辺の数だけ計算されるので辺の数M
+
 import sys
-import queue
+# import queue
 n,m = map(int,input().split())
-# ab = [list(map(int,input().split)) for _ in range(n)]
-ab = []
+ab = [[] for _ in range(n)]
+
 for s in sys.stdin.readlines():
-    ab.append(list(map(int,s.split())))
+    a,b = map(int,s.split())
+    ab[a-1] += [b-1]
+    ab[b-1] += [a-1]
 
-visited = set()
-q = queue.Queue()
-visited.add()
-q.put()
+step = [None] * n
+step[0] = 0
+next = []
+next.append(0)
+cnt = [0] * n
+cnt[0] = 1
 
-while not q.empty():
-    
+for v in next:
+    for vv in ab[v]:
+        # まだ探索してない
+        if step[vv] is None:
+            step[vv] = step[v]+1
+            cnt[vv] = cnt[v]
+            next.append(vv)
+        elif step[vv] == step[v]+1:
+                cnt[vv] += cnt[v]
+                cnt[vv] %= 10**9+7
+print(cnt[n-1])
+
+#! 速度が遅い
+# while not done:
+#     new_visited = set()
+#     for _ in range(next.qsize()):
+#         visit = next.get()
+
+#         for v in ab[visit]:
+#             if v == n:
+#                 ans+=1
+#                 done = True
+#             elif not v in visited:
+#                 new_visited.add(v)
+#                 next.put(v)
+#     visited = visited | new_visited
+#     if next.empty():
+#         done = True
+
+# print(ans)

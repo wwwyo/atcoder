2997cfc473bf04b9cabd8310b59b7ecf5809f7b4
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-10-03 01:28:19 +0900

    contest

--- contest/abc221/d.py
+++ contest/abc221/d.py
@@ -1,12 +1,26 @@
 n = int(input())
+cnt = []
+ans = [0] * (n+1)
+# ログインとログアウトの日付を別で管理する
+# ダメな例＝＞でやってたlist[(ログイン日,ログアウト日)]
+# ＊累積 accumulate
 
-memo = [0] * n
-# i日目の変化量をメモしていく
-ab = [list(map(int ,input().split())) for _ in range(n)]
-ab.sort()
-for i in range(n):
-    lst = ab[i]
-    memo[lst[0]] += 1
-    memo[lst[1]] -= 1
-lst.sort()
+for _ in range(n):
+    a,b = map(int,input().split())
+    start = a
+    end = a+b
+    cnt.append((start,+1))
+    cnt.append((end,-1))
 
+cnt.sort()
+
+user = 0
+last = -1
+# a ~ lastの間の日にちはユーザー数同じ
+for a,b in cnt:
+    ans[user] += a - last
+    last = a
+    user += b
+    # 最後は0人になるから今回は関係ない
+
+print(*ans[1:])

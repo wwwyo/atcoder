34cd44f65b3f3dd1a58645dc63c2323197bade2f
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-10-24 20:37:57 +0900

    abc224

--- contest/abc224/d.py
+++ contest/abc224/d.py
@@ -1,51 +1,45 @@
-from sys import setrecursionlimit
-setrecursionlimit(10**8)
-
-def is_correct(p):
-    for i in range(9):
-        if i != p[i]:
-            return False
-    return True
-
-def recursive(prev_p,cnt,emp_node,prev_node):
-    p = prev_p[:]
-    print(p)
-    if is_correct(p):
-        print(cnt)
-        exit()
-    for node in edge[emp_node]:
-        idx = edge[emp_node].index(node)
-        del edge[emp_node][idx]
-
-        if node == prev_node: continue
-        p[emp_node] ,p[node] = p[node],p[emp_node]
-        recursive(p,cnt+1,node,emp_node)
-
+from collections import deque
 
 if __name__ == '__main__':
     # max(m) = 36
+    # >>> math.factorial(9)
+    # 362880通り
+    n = 9
     m = int(input())
 
     # 頂点数9
-    edge = [[] for _ in range(10)]
+    edge = [[] for _ in range(n)]
     for _ in range(m):
-        u,v = map(int,input().split())
+        u,v = map(lambda x: int(x)-1,input().split())
         edge[u].append(v)
         edge[v].append(u)
 
     # コマjの初期値頂点
-    # 8
-    p = list(map(int,input().split()))
-    emp_node = (set(range(1,10)) - set(p)).pop()
-    # [node] = コマ
-    new_p = [0] * 10
+
+    p = list(map(lambda x:int(x)-1,input().split()))
+    goal = list(range(0,9))
+    goal[8] = -1
+    # s[頂点] = コマ
+    s = [-1] * 9
     for i,v in enumerate(p):
-        new_p[v] = i+1
-    recursive(new_p,0,emp_node, emp_node)
-    print(-1)
+        s[v] = i
 
+    q = deque([(s,0)])
 
+    visited = {tuple(s):0}
+    while len(q):
+        s,cnt = q.popleft()
+        cnt+=1
+        emp_idx = s.index(-1)
+        for node in edge[emp_idx]:
+            state = s[:]
+            state[node],state[emp_idx] = -1,state[node]
+            if visited.get(tuple(state)) == None:
+                visited[tuple(state)] = cnt
+                q.append((state,cnt))
 
-
-
+    if visited.get(tuple(goal)) != None:
+        print(visited[tuple(goal)])
+    else:
+        print(-1)
 

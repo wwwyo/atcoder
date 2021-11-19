8111be5f89de24942b24ba786c9050a3e0a3711b
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-11-19 12:25:08 +0900

    精門37

--- redcoder/37.py
+++ redcoder/37.py
@@ -1,1 +1,16 @@
+# 10^4,20
 n,m = map(int,input().split())
+
+c = sorted(list(map(int,input().split())))
+min_ = n
+dp = [i for i in range(n+1)]
+# dp[i]i円の時最小のコイン枚数
+for i in range(1,n+1):
+    for j in range(m):
+        if i < c[j]:
+            continue
+        dp[i] = min(dp[i], dp[i-c[j]]+1)
+# print(dp)
+print(dp[n])
+
+# 今i円までの最大値を考える

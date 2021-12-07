1d550544c000586b2f90b35ac44246872b840ddb
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-12-07 16:54:47 +0900

    arc131

--- contest/arc131/c.py
+++ contest/arc131/c.py
@@ -1,16 +1,28 @@
+"""
+2手後にゲームが終わるか
+
+2手後にゲームが終わるとする
+1手目に任意のiをとった時後手がゲームを終わらせるようなkを取る
+i=>kを取る逆も成りたつ1手目k=>2手目i
+このような組が成り立つのでN/2個存在する。
+=>奇数だと成り立たない
+偶数だと相手で終わる。
+奇数だと自分で終わる
+"""
 # n = 10**5
 n = int(input())
 a = list(map(int,input().split()))
 
-xor = a[0]^a[1]
-for i in range(n):
-    xor ^=a[i]
-    if xor != 0:
-        continue
-    if (n-i) %2 == 0:
-        print("")
-
-if xor in a:
+if n %2 == 1:
     print('Win')
+    exit()
+
+xor = a[0]
+for i in range(1,n):
+    xor ^=a[i]
+for i in range(n):
+    if xor^a[i] ==0:
+        print('Win')
+        break
 else:
     print('Lose')

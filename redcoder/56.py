b80fd0094ed82cab89870eb7d2317477133d667b
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-12-09 20:16:21 +0900

    redcoder56

--- redcoder/56.py
+++ redcoder/56.py
@@ -1,6 +1,23 @@
+import heapq
 # 10^5
 v,e,r = map(int,input().split())
 dist = [[] for _ in range(v)]
 for _ in range(e):
     s,t,d = map(int,input().split())
-    dist[s].append(t)
+    dist[s].append((t,d))
+
+seen = ["INF"] * v
+queue = [(0,r)]
+
+while len(queue):
+    curr_cost,current = heapq.heappop(queue)
+    if seen[current] != "INF":
+        continue
+    seen[current] = curr_cost
+    for next,next_cost in dist[current]:
+        if seen[next] != "INF":
+            continue
+        heapq.heappush(queue,(next_cost + curr_cost,next))
+
+print(*seen,sep="\n")
+

21280626caef42df2cf6f9c26cb03e3ca4eba86f
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-12-14 23:56:16 +0900

    redcoder 25

---
+++ redcoder/25.py
@@ -0,0 +1,46 @@
+from collections import deque
+
+# 列、行
+def main(w,h):
+    dist = []
+    for _ in range(h):
+        c = list(map(lambda x: 0 if x==1 else 0,input().split()))
+        dist.append(c)
+    visited = [[0]*w for _ in range(h)]
+    ans = 0
+    for i in range(h):
+        for j in range(w):
+            if visited[i][j]:
+                continue
+            bfs(i,j,dist)
+            ans+=1
+    print('---',ans)
+
+def bfs(i,j,visited):
+    queue = deque([[i,j]])
+    visited[i][j] = 1
+    d = [(0,1),(1,0),(1,1),(0,-1),(-1,0),(-1,-1),(-1,1),(1,-1)]
+
+    while queue:
+        c_i,c_j = queue.popleft()
+        for dx,dy in d:
+            x = c_i+dx
+            y = c_j+dy
+            if not(0<=x<h and 0<=y<w):
+                continue
+            if visited[x][y]:
+                continue
+            visited[x][y] = 1
+            queue.append([x,y])
+    
+        
+
+
+if __name__ == '__main__':
+    while True:
+        w,h = map(int,input().split())
+        if w == 0 and h == 0:
+            break
+        main(w,h)
+
+

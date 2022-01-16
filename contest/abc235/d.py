dfb27c0c2c8ecea21d3b831414e8d619fff44da5
yuito118 <72675978+yuito118@users.noreply.github.com>
2022-01-16 16:26:34 +0900

    contestabc235

--- contest/abc235/d.py
+++ contest/abc235/d.py
@@ -1,69 +1,101 @@
 
-import math
-import heapq
+# import math
+# import heapq
 
-# 10**6
+# # 10**6
+# a,n = map(int,input().split())
+
+# """
+# 1. x * a
+# 2. x[-1] = x[0]
+# *末尾が0じゃない or 2桁以上
+# """
+
+# ans = 1
+# cnt = 0
+# ans_cnt = math.inf
+# answers = []
+# heapq.heapify(answers)
+# memo = {}
+# lim = len(str(n))
+# # >>>n
+# while ans <= n:
+#     if ans == n:
+#         ans_cnt = cnt
+#         break
+#     ans *= a
+#     cnt += 1
+#     if ans >= 10:
+#         heapq.heappush(answers, [cnt, ans])
+#         memo[ans] = cnt
+
+# def is_ok(ans_,cnt_):
+#     return len(str(ans_)) <= lim and (not memo.get(ans_) or memo[ans_] > cnt_)
+
+# while answers:
+#     cnt,ans = heapq.heappop(answers)
+#     # print(cnt,ans)
+#     if ans == n:
+#         print(min(ans_cnt, cnt))
+#         exit()
+#     # elif ans > n:
+#     #     continue
+#     if is_ok(ans *a,cnt+1):
+#         heapq.heappush(answers, [cnt+1,ans*a])
+#         # print('??',ans*a)
+#         memo[ans*a] = cnt+1
+#     # 逆転
+#     ans_s = list(str(ans))
+#     if ans_s[-1] == '0':
+#         continue
+#     ans_last = ans_s.pop()
+#     ans = int(ans_last + ''.join(ans_s))
+#     # print('--',ans)
+#     if is_ok(ans,cnt+1):
+#         heapq.heappush(answers, [cnt+1,ans])
+#         memo[ans] = cnt+1
+
+#     # if not memo.get(ans):
+#     #     memo[ans] = 1
+#     #     answers.append((ans, cnt+1))
+#     #     answers.append((ans+a, cnt+2))
+
+# if ans_cnt == math.inf:
+#     print(-1)
+# else:
+#     print(ans_cnt)
+
+
+
+# bfsすれば最初にでできたnodeが最小
+from collections import deque
 a,n = map(int,input().split())
+lim = 10**len(str(n))
+costs = [-1] * lim
+q = deque()
+q.append(1)
+costs[1] = 0
 
-"""
-1. x * a
-2. x[-1] = x[0]
-*末尾が0じゃない or 2桁以上
-"""
+while q:
+    node = q.popleft()
+    cost = costs[node]
 
-ans = 1
-cnt = 0
-ans_cnt = math.inf
-answers = []
-heapq.heapify(answers)
-memo = {}
-lim = len(str(n))
-# >>>n
-while ans <= n:
-    if ans == n:
-        ans_cnt = cnt
-        break
-    ans *= a
-    cnt += 1
-    if ans >= 10:
-        heapq.heappush(answers, [cnt, ans])
-        memo[ans] = cnt
+    node_multi = node * a
+    if node_multi < lim and costs[node_multi] == -1:
+        costs[node_multi] = cost + 1
+        q.append(node_multi)
 
-def is_ok(ans_,cnt_):
-    return len(str(ans_)) <= lim and (not memo.get(ans_) or memo[ans_] > cnt_)
+    # 入れ替え
+    node_str = list(str(node))
+    node_last = node_str.pop()
+    if node_last != "0" and len(node_str)>0:
+        node_change = int(node_last + ''.join(node_str))
+        if node_change < lim and costs[node_change] == -1:
+            costs[node_change] = cost +1
+            q.append(node_change)
+print(costs[n])
 
-while answers:
-    cnt,ans = heapq.heappop(answers)
-    # print(cnt,ans)
-    if ans == n:
-        print(min(ans_cnt, cnt))
-        exit()
-    # elif ans > n:
-    #     continue
-    if is_ok(ans *a,cnt+1):
-        heapq.heappush(answers, [cnt+1,ans*a])
-        # print('??',ans*a)
-        memo[ans*a] = cnt+1
-    # 逆転
-    ans_s = list(str(ans))
-    if ans_s[-1] == '0':
-        continue
-    ans_last = ans_s.pop()
-    ans = int(ans_last + ''.join(ans_s))
-    # print('--',ans)
-    if is_ok(ans,cnt+1):
-        heapq.heappush(answers, [cnt+1,ans])
-        memo[ans] = cnt+1
 
-    # if not memo.get(ans):
-    #     memo[ans] = 1
-    #     answers.append((ans, cnt+1))
-    #     answers.append((ans+a, cnt+2))
-
-if ans_cnt == math.inf:
-    print(-1)
-else:
-    print(ans_cnt)
 
 
 
--- contest/abc235/e.py
+++ contest/abc235/e.py
@@ -1,67 +1,121 @@
 
 import sys
-sys.setrecursionlimit(10**7)
-# 10**5
+sys.setrecursionlimit(10**5)
+# # 10**5
+# n,m,q = map(int,input().split())
+# dist = []
+# for _ in range(m):
+#     a,b,c = map(int,input().split())
+#     dist.append((c,a-1,b-1))
+# dist.sort()
+
+# class UnionFind:
+#     def __init__(self,n):
+#         self.n = n
+#         self.parents = [i for i in range(n)]
+#         self.memo = {}
+
+#     def find(self,x):
+#         if self.parents[x] == x:
+#             return x
+#         self.parents[x] = self.find(self.parents[x])
+#         return self.parents[x]
+
+#     def unite(self,x,y, cost):
+#         x = self.find(x)
+#         y = self.find(y)
+#         if x == y:
+#             return
+#         else:
+#             self.parents[x] = self.parents[y]
+#             self.memo[(x,y)] = cost
+
+#     def same(self,x,y):
+#         return self.find(x) == self.find(y)
+
+# uf = UnionFind(n)
+# # cost = 0
+# dist_num = 0
+# for i in range(m):
+#     c,a,b = dist[i]
+#     if uf.same(a,b):
+#         continue
+#     uf.unite(a,b,c)
+#     # cost += c
+#     dist_num += 1
+#     if dist_num >= n:
+#         break
+
+# print('u')
+# for _ in range(q):
+#     print(q)
+#     u,v,w = map(int,input().split())
+#     u-=1
+#     v-=1
+#     if uf.memo[u,v] > w:
+#         print('Yes')
+#     else:
+#         print('No')
+
+
+
 n,m,q = map(int,input().split())
-dist = []
-for _ in range(m):
-    a,b,c = map(int,input().split())
-    dist.append((c,a-1,b-1))
-dist.sort()
 
-class UnionFind:
+class UnionFind():
     def __init__(self,n):
-        self.n = n
-        self.parents = [i for i in range(n)]
-        self.memo = {}
+        # 根が要素数を管理
+        self.p = [-1] * (n+1)
 
-    def find(self,x):
-        if self.parents[x] == x:
-            return x
-        self.parents[x] = self.find(self.parents[x])
-        return self.parents[x]
+    def find(self, x):
+        if self.p[x] < 0: return x
+        self.p[x] = self.find(self.p[x])
+        return self.p[x]
 
-    def unite(self,x,y, cost):
+    def union(self, x,y):
+
         x = self.find(x)
         y = self.find(y)
         if x == y:
             return
-        else:
-            self.parents[x] = self.parents[y]
-            self.memo[(x,y)] = cost
 
+        # 要素数が多い方の根に結合
+        if self.p[x] > self.p[y]:
+            x,y = y,x
+        # -に増えていく
+        self.p[x] += self.p[y]
+        self.p[y] = x
+
+    
     def same(self,x,y):
+        # print('same!')
         return self.find(x) == self.find(y)
 
-uf = UnionFind(n)
-# cost = 0
-dist_num = 0
-for i in range(m):
-    c,a,b = dist[i]
-    if uf.same(a,b):
-        continue
-    uf.unite(a,b,c)
-    # cost += c
-    dist_num += 1
-    if dist_num >= n:
-        break
 
-print('u')
-for _ in range(q):
-    print(q)
+edges = []
+for _ in range(m):
     u,v,w = map(int,input().split())
-    u-=1
-    v-=1
-    if uf.memo[u,v] > w:
+    edges.append((w,u,v,-1))
+
+for i in range(q):
+    u,v,w = map(int,input().split())
+    edges.append((w,u,v,i))
+
+edges.sort()
+
+ans = [False] * q
+uf = UnionFind(n)
+for (w, u,v,q) in edges:
+    # print(w,u,v,q)
+    if uf.same(u,v):
+        continue
+
+    if q != -1:
+        ans[q] = True
+    else:
+        uf.union(u,v)
+
+for a in ans:
+    if a:
         print('Yes')
     else:
         print('No')
-
-
-
-
-
-
-
-
-
